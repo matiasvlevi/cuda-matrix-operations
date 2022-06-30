274707a82ddc257375a515eeab2f8b6bf21adfe4
matiasvlevi <matiasvlevi@gmail.com>
2022-06-30 06:44:04 +0000

    new build script

--- build.sh
+++ build.sh
@@ -5,4 +5,21 @@ then
     mkdir ./build
 fi
 
-nvcc ./matrix/*.cu ./main.cu -o ./build/main
+build_examples () {
+    nvcc ./matrix/*.cu ./examples/transpose_example.cu -o ./build/transpose_example
+    echo "built transpose_example"
+    nvcc ./matrix/*.cu ./examples/add_example.cu -o ./build/add_example
+    echo "built add_example"
+}
+
+build_main () {
+    nvcc ./matrix/*.cu ./main.cu -o ./build/main
+    echo "built main"
+}
+
+if [[ $1 -eq "--examples" ]] || [[ $1 -eq "-e" ]];
+then 
+    build_examples;
+fi
+
+build_main
--- build_examples.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ ! -d "./build" ]; 
-then
-    mkdir ./build
-fi
-
-nvcc ./matrix/*.cu ./examples/transpose_example.cu -o ./build/transpose_example
-nvcc ./matrix/*.cu ./examples/add_example.cu -o ./build/add_example
